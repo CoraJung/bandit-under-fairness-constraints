5dd38921cd81095e8eb630813219fb9c67602a0b
emmyphung <emmy.phungt@gmail.com>
2021-04-25 21:55:16 -0400

    Update chosen covariates + turn all cat features to dummies

--- Python/datasets/noda_bandit.py
+++ Python/datasets/noda_bandit.py
@@ -11,11 +11,19 @@ from utils import keyboard
 DATA_PATH = "/misc/vlgscratch5/PichenyGroup/s2i-common/bandit-under-fairness-constraints/Python/datasets/noda/noda_trial.csv"
 
 # Need to determine X
-LABELS_TO_KEEP = np.array(['SEX_DFDN', 'CHARGE_CLASS', 'RACE_DFDN']) #use our cals
+CAT_TO_KEEP = ['DISP_CODE_SENT', 'SENTENCE_TYPE', 'HABITUAL_OFFENDER_FLAG_SENT', 'SENTENCE_LOCATION', \
+                 'CUSTODY_CODE', 'CRIMINAL_FLAG', 'SEX_DFDN', 'RACE_DFDN', 'JUVENILE_INVOLVED_FLAG', 'RACE_TRIAL_ADA', \
+                'SEX_TRIAL_ADA', 'PARTY_TRIAL_ADA', 'SEX_SCREEN_ADA', 'PARTY_SCREEN_ADA', 'BOND_MADE_FLAG', \
+                'CHARGE_CAT', 'BOND_TYPE_CODE', 'SEX_JUDGE', 'RACE_JUDGE','PARTY_JUDGE', 'RACE_SCREEN_ADA' ]
+NUM_TO_KEEP = ['FINE_AMOUNT', 'NBR_OF_DFDN', 'AGE_DFDN', 'AGE_DFDN_ISNA', 'CASE_CLASS_ISNA', \
+                'BOND_MADE_AMOUNT', 'BOND_SET_AMOUNT', 'CHARGE_CLASS', 'AGE_JUDGE', 'DOB_SCREEN_ADA', 'DOB_TRIAL_ADA', \
+                'AGE_JUDGE_ISNA', 'AGE_SCREEN_ADA_ISNA', 'AGE_TRIAL_ADA_ISNA', 'CHARGE_CLASS_ISNA']
+
+LABELS_TO_KEEP = CAT_TO_KEEP + NUM_TO_KEEP
 
 def load(r_train=0.4, r_candidate=0.2, T0='W', T1='B', seed=None, include_T=False, include_intercept=True, use_pct=1.0, use_score_text=False, rwd_recid=-1.0, rwd_nonrecid=1.0, use_cached_gps=False):
 	random = np.random.RandomState(seed)
-	scores = pd.read_csv(DATA_PATH)
+	scores = pd.read_pickle(DATA_PATH)
 	# Generate the full dataset
 	#S = scores[scores['RACE_DFDN'].isin([T0,T1])].copy()
 	S = scores[np.logical_or(scores['RACE_DFDN']==T0, scores['RACE_DFDN']==T1)].copy() 
@@ -30,10 +38,10 @@ def load(r_train=0.4, r_candidate=0.2, T0='W', T1='B', seed=None, include_T=Fals
 
 	S = S[LABELS_TO_KEEP]
 
-	# Turn some covariates to one-hot encoding -> may need more depending on X
-	S = with_dummies(S, 'SEX_DFDN') 
-	S = with_dummies(S, 'CHARGE_CLASS')
-
+	# Turn all cat covariates to one-hot encoding -> do race separately
+	for col in CAT_TO_KEEP:
+		if col != 'RACE_DFDN':
+			S = with_dummies(S, col)
 
 	T = 1 * (S['RACE_DFDN']==T1).values #One-hot encoding for race T1
 	del S['RACE_DFDN']
